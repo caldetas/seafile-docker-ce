02f364875b3e9cf01d1d31332ec31646b38f6c09
h44z <christoph.haas@prolicht.at>
2025-11-01 21:42:05 +0100

    Update to Seafile-CE 13.0.12 (#2)
    * update to seafile ce 13
    
    (cherry picked from commit 579427fe237d50318d4dad96f2ef70c5e79843bf)

--- restore.sh
+++ restore.sh
@@ -1,53 +1,52 @@
   #!/usr/bin/env bash
   set -euo pipefail
 
+  #the restore directory ($RESTORE_DIR) should contain these two folders from the backup:
+  #    - seafile-data
+  #    - seafile-docker-ce
+
   # first reinstall seafile fresh (delete git folder and rebuild)
   # second uncomment the external storage line in docker-compose
   # third run sudo ./setup.sh
 
-  SCRIPT_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
-  RESTORE_DIR="/mnt/backup/nixcz/restore"
-  DATA_DIR="$SCRIPT_DIR/data/seafile"
-  SEAFILE_DATA_BLOCKS_DIR="/mnt/nas/seafile-data" #if actual data is stored on external mount
+  SCRIPT_DIR="/home/caldetas/git/seafile-docker-ce"
+  RESTORE_DIR="/tmp/restore"
   cd "$SCRIPT_DIR"
 
   set -a; source  /run/secrets/seafile/.env; set +a
 
   echo "Starting restore process..."
 
-  echo $RESTORE_DIR$SCRIPT_DIR
-  echo $RESTORE_DIR$DATA_DIR
-  echo $SEAFILE_DATA_BLOCKS_DIR
-
-
-  mkdir -p $RESTORE_DIR
-  mkdir -p $SEAFILE_DATA_BLOCKS_DIR
-  rm -fr $RESTORE_DIR/* || true
-  borgmatic extract --archive latest --destination $RESTORE_DIR
-
+  echo $SCRIPT_DIR
+  echo $RESTORE_DIR
 
 # Restore database dumps
 echo "Restoring MySQL databases..."
-docker compose exec -T db mysql -u root --password="$DB_ROOT_PASSWD" ccnet_db < $RESTORE_DIR$SCRIPT_DIR/backup/db/ccnet.sql
-docker compose exec -T db mysql -u root --password="$DB_ROOT_PASSWD" seafile_db < $RESTORE_DIR$SCRIPT_DIR/backup/db/seafile.sql
-docker compose exec -T db mysql -u root --password="$DB_ROOT_PASSWD" seahub_db < $RESTORE_DIR$SCRIPT_DIR/backup/db/seahub.sql
-
+docker compose exec -T db mysql -u root --password="$DB_ROOT_PASSWD" ccnet_db < $RESTORE_DIR/seafile-docker-ce/backup/db/ccnet.sql
+docker compose exec -T db mysql -u root --password="$DB_ROOT_PASSWD" seafile_db < $RESTORE_DIR/seafile-docker-ce/backup/db/seafile.sql
+docker compose exec -T db mysql -u root --password="$DB_ROOT_PASSWD" seahub_db < $RESTORE_DIR/seafile-docker-ce/backup/db/seahub.sql
 echo "Database restore complete."
+
+
+systemctl stop seafile-docker-compose.service
+
 
 # Restore seafile data
 echo "Restoring seafile data directories..."
-rsync -aHr $RESTORE_DIR$DATA_DIR/ $DATA_DIR/
-echo "Restoring seafile data blocks directories..."
+rsync -aHr $RESTORE_DIR/seafile-docker-ce/data/seafile/ $SCRIPT_DIR/data/seafile/ #settings and structure folder (eg. library permissions)
+rsync -aHr $RESTORE_DIR/seafile-data/ $SCRIPT_DIR/data/seafile/seafile-data # data blocks
+echo "Finished restoring seafile data blocks directories..."
 
-rsync -aHr $RESTORE_DIR$SEAFILE_DATA_BLOCKS_DIR/ $SEAFILE_DATA_BLOCKS_DIR #$DATA_DIR/seafile-data/ for local install
-chown -R root:root $SEAFILE_DATA_BLOCKS_DIR #only necessary with external block storage
+#needs root as owner
+sudo chown -R root:root $SCRIPT_DIR/data
 
-chown -R root:root $DATA_DIR
-rm -fr $RESTORE_DIR/* || true
+# external storage needs root as owner
+#rsync -aHr $RESTORE_DIR$SEAFILE_DATA_BLOCKS_DIR/ $DATA_DIR/
+#chown -R root:root $SEAFILE_DATA_BLOCKS_DIR #only necessary with external block storage
+#chown -R root:root $DATA_DIR
 
 echo "Data restore complete. Restarting Docker Compose.."
 
-systemctl stop seafile-docker-compose.service
 systemctl start seafile-docker-compose.service
 
 echo "Restoration complete. Exiting.."
